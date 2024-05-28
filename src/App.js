f40e10e302240c7f10efa9bc0cfbb2f42277a044
DevAyush01 <Ayushchoudharry00@gmail.com>
2024-05-28 17:31:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import { FormControl, InputLabel , Input } from '@mui/material';
 import './App.css';
+import Message from './Message';
 
 function App() {
+  const [input,setInput] = useState('');
+  const [messages,setMessages] = useState([{username: 'Ayush',text: 'Hey'},
+    {username: 'Vinayak',text : 'Hello '}
+  ]);
+  const [username,setUsername] = useState('');
+
+  useEffect(()=>{
+    setUsername(prompt('Enter your name'));
+  },[])
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage= (event)=>{
+       event.preventDefault();
+        setMessages([...messages, {username : username,text: input}]);
+        setInput('')
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Hello Programmers!</h1>
+       <h3>Welcome {username}</h3>
+       <form>
+       <FormControl>
+         <InputLabel>Enter message...</InputLabel>
+         <Input value={input} onChange={event=>setInput(event.target.value)}/>
+         <Button disabled={!input} type='submit' variant='contained' endIcon={<SendIcon />} onClick={sendMessage}>Send</Button>
+       </FormControl>
+       </form>
+
+
+      <div>
+      {
+      messages.map( message =>(
+        <Message username={username} message={message}/>
+      ))
+      }
+      </div>
     </div>
   );
 }
